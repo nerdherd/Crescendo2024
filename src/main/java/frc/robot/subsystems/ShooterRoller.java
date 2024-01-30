a4c552f86f8a13c8b7d3fa1c1e700c1136906c41
Kyle Huang <kylehuang06@gmail.com>
2024-01-30 09:53:31 -0800

    add missing import

--- src/main/java/frc/robot/subsystems/ShooterRoller.java
+++ src/main/java/frc/robot/subsystems/ShooterRoller.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ModuleConstants;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.SuperStructureConstants;
 
