b342255b77de2034d1a8f3e897fcc97ab4d5815a
Julianna Salonga <98715786+julivnna@users.noreply.github.com>
2024-02-12 18:58:52 -0800

    Updated Linear Actuator
    Co-Authored-By: MBritto <109992316+mb-coding@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/LinearActuator.java
+++ src/main/java/frc/robot/subsystems/LinearActuator.java
@@ -1,9 +1,6 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -15,14 +12,21 @@ public class LinearActuator extends SubsystemBase {
     public LinearActuator () {
         CommandScheduler.getInstance().registerSubsystem(this);
         linearActuator = new Servo(0);
-        SmartDashboard.putNumber("Extended", 0);
     }
 
     public void retract() {
-        linearActuator.set(0);
+        linearActuator.set(0.2); // if pos is too low, it won't move
+    }
+
+    public void extend() {
+        linearActuator.set(0.8); // if pos is too high, it won't move
     }
 
     public Command retractCommand() {
         return Commands.runOnce(this::retract);
+    }
+
+    public Command extendCommand() {
+        return Commands.runOnce(this::extend);
     }
 }
