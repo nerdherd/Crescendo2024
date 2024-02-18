0665239215f32fc6538114e0ee8837617289fb9a
Zachary Miller <superz006@icloud.com>
2024-02-17 17:18:37 -0800

    Pathplanner named command testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,9 @@ package frc.robot;
 
 import java.util.List;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.PowerDistribution;
@@ -17,10 +20,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import frc.robot.Constants.ControllerConstants;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.commands.SwerveJoystickCommand;
+import frc.robot.commands.TowSwerve;
 import frc.robot.commands.autos.Auto4Notes;
 import frc.robot.commands.autos.Auto4NotesDirect;
 import frc.robot.commands.autos.AutoMidNotes;
@@ -94,6 +99,7 @@ public class RobotContainer {
 
     apriltagCamera.toggleLight(false);
 
+    initNamedCommands();
     initAutoChoosers();
     initShuffleboard();
 
@@ -228,70 +234,82 @@ public class RobotContainer {
 
     commandOperatorController.share().whileTrue(superSystem.linearActuator.retractCommand());
   }
+  
+  private void initNamedCommands() {
+    NamedCommands.registerCommand("IntakeDirectShoot", superSystem.intakeDirectShoot());
+    NamedCommands.registerCommand("Wait1Sec", new WaitCommand(1));
+    NamedCommands.registerCommand("TowSwerve", new TowSwerve(swerveDrive));
+  }
 
   private void initAutoChoosers() {
   	List<String> paths = AutoBuilder.getAllAutoNames();
     autoChooser.addOption("Do Nothing", Commands.none());
     
-    if (paths.contains("4PAuto")) {
-      autoChooser.addOption("4PAuto", new Auto4Notes(swerveDrive, "4PAuto", superSystem));
-      autoChooser.addOption("4PAuto Direct", new Auto4NotesDirect(swerveDrive, "4PAuto", superSystem));
-    }
-    if (paths.contains("Basic2Piece")) {
-      autoChooser.addOption("Basic2Piece", new Basic2Piece(swerveDrive, "Basic2Piece", superSystem));
-    }
-
-    if (paths.contains("Basic2PiecePos2")) {
-      autoChooser.addOption("Basic2PiecePos2", new Basic2Piece(swerveDrive, "Basic2PiecePos2", superSystem));
-    }
-
-    if (paths.contains("Basic3Piece")) {
-      autoChooser.addOption("Basic3Piece", new Basic3Piece(swerveDrive, "Basic3Piece", superSystem));
-    }
-
-    if (paths.contains("Basic4Piece")) {
-      autoChooser.setDefaultOption("Basic4Piece", new Basic4Piece(swerveDrive, "Basic4Piece", superSystem));
-    }
-
-    if (paths.contains("Basic4PieceSeparated")) {
-      autoChooser.setDefaultOption("Basic4PieceSeparated", new Basic4PieceSeparated(swerveDrive, "Basic4PieceSeparated", superSystem));
-    }
-
-    // if (paths.contains("Basic5Piece")) {
-    //   autoChooser.addOption("Basic5Piece", new Basic5Piece(swerveDrive, "Basic5Piece", superSystem));
+    // if (paths.contains("4PAuto")) {
+    //   autoChooser.addOption("4PAuto", new Auto4Notes(swerveDrive, "4PAuto", superSystem));
+    //   autoChooser.addOption("4PAuto Direct", new Auto4NotesDirect(swerveDrive, "4PAuto", superSystem));
+    // }
+    // if (paths.contains("Basic2Piece")) {
+    //   autoChooser.addOption("Basic2Piece", new Basic2Piece(swerveDrive, "Basic2Piece", superSystem));
     // }
 
-    if (paths.contains("Basic3PiecePos2")) {
-      autoChooser.addOption("Basic3PiecePos2", new Basic3Piece(swerveDrive, "Basic3PiecePos2", superSystem));
-    }
+    // if (paths.contains("Basic2PiecePos2")) {
+    //   autoChooser.addOption("Basic2PiecePos2", new Basic2Piece(swerveDrive, "Basic2PiecePos2", superSystem));
+    // }
 
-    if (paths.contains("Basic3PieceV2")) {
-      autoChooser.addOption("Basic3PieceV2", new Basic3Piece(swerveDrive, "Basic3PieceV2", superSystem));
-    }
-    if (paths.contains("Test1M")) {
-      autoChooser.addOption("Test1M", new Test1M(swerveDrive));
-    }
-    if (paths.contains("Test1MBack")) {
-      autoChooser.addOption("Test1MBack", new Test1MBack(swerveDrive));
-    }
-    if (paths.contains("Basic6PieceSeparated")) {
-      autoChooser.addOption("Basic6PieceSeparated", new Basic6PieceSeparated(swerveDrive, "Basic6PieceSeparated", superSystem));
-    }
-    if (paths.contains("OneMeterSquareAuto")) {
-      autoChooser.addOption("OneMeterSquareAuto", new OneMeterSquareAuto(swerveDrive, "OneMeterSquareAuto"));
-    }
-    if (paths.contains("TwoMeterSquareAuto")) {
-      autoChooser.addOption("TwoMeterSquareAuto", new TwoMeterSquareAuto(swerveDrive, "TwoMeterSquareAuto"));
-    }
-    if (paths.contains("ThreeMeterSquareAuto")) {
-      autoChooser.addOption("ThreeMeterSquareAuto", new ThreeMeterSquareAuto(swerveDrive, "ThreeMeterSquareAuto"));
-    }
-    if (paths.contains("RotateSquareAuto")) {
-      autoChooser.addOption("RotateSquareAuto", new RotateSquareAuto(swerveDrive, "RotateSquareAuto"));
-    }
-    if (paths.contains("Mid4Notes")) {
-      autoChooser.addOption("Mid4Notes with Vision", new AutoMidNotes(swerveDrive, "Mid4Notes", superSystem, noteCamera));
-    }
+    // if (paths.contains("Basic3Piece")) {
+    //   autoChooser.addOption("Basic3Piece", new Basic3Piece(swerveDrive, "Basic3Piece", superSystem));
+    // }
+
+    // if (paths.contains("Basic4Piece")) {
+    //   autoChooser.setDefaultOption("Basic4Piece", new Basic4Piece(swerveDrive, "Basic4Piece", superSystem));
+    // }
+
+    // if (paths.contains("Basic4PieceSeparated")) {
+    //   autoChooser.setDefaultOption("Basic4PieceSeparated", new Basic4PieceSeparated(swerveDrive, "Basic4PieceSeparated", superSystem));
+    // }
+
+    // // if (paths.contains("Basic5Piece")) {
+    // //   autoChooser.addOption("Basic5Piece", new Basic5Piece(swerveDrive, "Basic5Piece", superSystem));
+    // // }
+
+    // if (paths.contains("Basic3PiecePos2")) {
+    //   autoChooser.addOption("Basic3PiecePos2", new Basic3Piece(swerveDrive, "Basic3PiecePos2", superSystem));
+    // }
+
+    // if (paths.contains("Basic3PieceV2")) {
+    //   autoChooser.addOption("Basic3PieceV2", new Basic3Piece(swerveDrive, "Basic3PieceV2", superSystem));
+    // }
+    // if (paths.contains("Test1M")) {
+    //   autoChooser.addOption("Test1M", new Test1M(swerveDrive));
+    // }
+    // if (paths.contains("Test1MBack")) {
+    //   autoChooser.addOption("Test1MBack", new Test1MBack(swerveDrive));
+    // }
+    // if (paths.contains("Basic6PieceSeparated")) {
+    //   autoChooser.addOption("Basic6PieceSeparated", new Basic6PieceSeparated(swerveDrive, "Basic6PieceSeparated", superSystem));
+    // }
+    // if (paths.contains("OneMeterSquareAuto")) {
+    //   autoChooser.addOption("OneMeterSquareAuto", new OneMeterSquareAuto(swerveDrive, "OneMeterSquareAuto"));
+    // }
+    // if (paths.contains("TwoMeterSquareAuto")) {
+    //   autoChooser.addOption("TwoMeterSquareAuto", new TwoMeterSquareAuto(swerveDrive, "TwoMeterSquareAuto"));
+    // }
+    // if (paths.contains("ThreeMeterSquareAuto")) {
+    //   autoChooser.addOption("ThreeMeterSquareAuto", new ThreeMeterSquareAuto(swerveDrive, "ThreeMeterSquareAuto"));
+    // }
+    // if (paths.contains("RotateSquareAuto")) {
+    //   autoChooser.addOption("RotateSquareAuto", new RotateSquareAuto(swerveDrive, "RotateSquareAuto"));
+    // }
+    // if (paths.contains("Mid4Notes")) {
+    //   autoChooser.addOption("Mid4Notes with Vision", new AutoMidNotes(swerveDrive, "Mid4Notes", superSystem, noteCamera));
+    // }
+
+    
+    // if (paths.contains("Basic4PieceNamedCommand")) {
+    //   autoChooser.addOption("Basic4NamedCommand", (AutoBuilder.followPath(null)PathPlannerAuto.getPathGroupFromAutoFile("Basic4PieceNamedCommand")));
+    // }
+
 
     ShuffleboardTab autosTab = Shuffleboard.getTab("Autos");
 
