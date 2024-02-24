94d8c4518e981e424ed51f52dc779cee03795411
Zachary Miller <93176330+londonwafflez@users.noreply.github.com>
2024-02-23 18:00:40 -0800

    Testing - Stable commit for drive practice
    Co-Authored-By: Brianna Adewinmbi <72527718+adewinmbi@users.noreply.github.com>
    Co-Authored-By: Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
    Co-Authored-By: Kylie <123788168+kymc-6@users.noreply.github.com>
    Co-Authored-By: silverx257 <109992135+silverx257@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -2,10 +2,13 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants.ColorSensorConstants;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 import com.revrobotics.ColorSensorV3;
 
 
-public class ColorSensor {
+public class ColorSensor implements Reportable {
     private final I2C.Port i2cPort;
     private final ColorSensorV3 colorSensor;
 
@@ -18,11 +21,22 @@ public class ColorSensor {
 
     public boolean noteIntook() {
         proximity = colorSensor.getProximity();
-        if (proximity < ColorSensorConstants.inProximity){
+        if (proximity > ColorSensorConstants.inProximity){
             return true;
         } 
         else {
             return false;
         }
     }
+
+    @Override
+    public void reportToSmartDashboard(LOG_LEVEL priority) {}
+
+    @Override
+    public void initShuffleboard(LOG_LEVEL priority) {
+        ShuffleboardTab tab = Shuffleboard.getTab("Indexer");
+        tab.addBoolean("Note Detected", this::noteIntook);
+        tab.addNumber("Distance Detected", () -> colorSensor.getProximity());
+    }
+    
 }
