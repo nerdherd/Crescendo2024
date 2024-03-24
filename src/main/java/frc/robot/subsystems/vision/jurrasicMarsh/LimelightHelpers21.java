cff62125a339bcd739b9defc6577e72bee3f1c29
Jim Duan <yajingduan@gmail.com>
2024-03-23 23:00:18 -0700

    Limelight upgrading

--- src/main/java/frc/robot/subsystems/vision/jurrasicMarsh/LimelightHelpers21.java
+++ src/main/java/frc/robot/subsystems/vision/jurrasicMarsh/LimelightHelpers21.java
@@ -1,10 +1,11 @@
-//LimelightHelpers v1.3.1 (March 4, 2024)
+//LimelightHelpers v1.2.1 (March 1, 2023)
 
 package frc.robot.subsystems.vision.jurrasicMarsh;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -303,18 +304,6 @@ public class LimelightHelpers {
         @JsonProperty("botpose_wpiblue")
         public double[] botpose_wpiblue;
 
-        @JsonProperty("botpose_tagcount")
-        public double botpose_tagcount;
-       
-        @JsonProperty("botpose_span")
-        public double botpose_span;
-       
-        @JsonProperty("botpose_avgdist")
-        public double botpose_avgdist;
-       
-        @JsonProperty("botpose_avgarea")
-        public double botpose_avgarea;
-
         @JsonProperty("t6c_rs")
         public double[] camerapose_robotspace;
 
@@ -374,35 +363,9 @@ public class LimelightHelpers {
     public static class LimelightResults {
         @JsonProperty("Results")
         public Results targetingResults;
-        
-        public String error;
 
         public LimelightResults() {
             targetingResults = new Results();
-            error = "";
-        }
-
-
-    }
-
-    public static class PoseEstimate {
-        public Pose2d pose;
-        public double timestampSeconds;
-        public double latency;
-        public int tagCount;
-        public double tagSpan;
-        public double avgTagDist;
-        public double avgTagArea;
-
-
-        public PoseEstimate(Pose2d pose, double timestampSeconds, double latency, int tagCount, double tagSpan, double avgTagDist, double avgTagArea) {
-            this.pose = pose;
-            this.timestampSeconds = timestampSeconds;
-            this.latency = latency;
-            this.tagCount = tagCount;
-            this.tagSpan = tagSpan;
-            this.avgTagDist = avgTagDist;
-            this.avgTagArea = avgTagArea;
         }
     }
 
@@ -421,9 +384,13 @@ public class LimelightHelpers {
     }
 
     private static Pose3d toPose3D(double[] inData){
+        if(inData == null) {
+            DriverStation.reportError("Bot Pose is Null!", null);
+            return new Pose3d();
+        }
         if(inData.length < 6)
         {
-            //System.err.println("Bad LL 3D Pose Data!");
+            System.err.println("Bad LL 3D Pose Data!");
             return new Pose3d();
         }
         return new Pose3d(
@@ -435,34 +402,12 @@ public class LimelightHelpers {
     private static Pose2d toPose2D(double[] inData){
         if(inData.length < 6)
         {
-            //System.err.println("Bad LL 2D Pose Data!");
+            System.err.println("Bad LL 2D Pose Data!");
             return new Pose2d();
         }
         Translation2d tran2d = new Translation2d(inData[0], inData[1]);
         Rotation2d r2d = new Rotation2d(Units.degreesToRadians(inData[5]));
         return new Pose2d(tran2d, r2d);
-    }
-
-    private static double extractBotPoseEntry(double[] inData, int position){
-        if(inData.length < position+1)
-        {
-            return 0;
-        }
-        return inData[position];
-    }
-
-    private static PoseEstimate getBotPoseEstimate(String limelightName, String entryName) {
-        var poseEntry = LimelightHelpers.getLimelightNTTableEntry(limelightName, entryName);
-        var poseArray = poseEntry.getDoubleArray(new double[0]);
-        var pose = toPose2D(poseArray);
-        double latency = extractBotPoseEntry(poseArray,6);
-        int tagCount = (int)extractBotPoseEntry(poseArray,7);
-        double tagSpan = extractBotPoseEntry(poseArray,8);
-        double tagDist = extractBotPoseEntry(poseArray,9);
-        double tagArea = extractBotPoseEntry(poseArray,10);
-        //getlastchange() in microseconds, ll latency in milliseconds
-        var timestamp = (poseEntry.getLastChange() / 1000000.0) - (latency/1000.0);
-        return new PoseEstimate(pose, timestamp,latency,tagCount,tagSpan,tagDist,tagArea);
     }
 
     public static NetworkTable getLimelightNTTable(String tableName) {
@@ -604,8 +549,8 @@ public class LimelightHelpers {
         return getLimelightNTDouble(limelightName, "tid");
     }
 
-    public static String getNeuralClassID(String limelightName) {
-        return getLimelightNTString(limelightName, "tclass");
+    public static double getNeuralClassID(String limelightName) {
+        return getLimelightNTDouble(limelightName, "tclass");
     }
 
     /////
@@ -665,17 +610,6 @@ public class LimelightHelpers {
     }
 
     /**
-     * Gets the Pose2d and timestamp for use with WPILib pose estimator (addVisionMeasurement) when you are on the BLUE
-     * alliance
-     * 
-     * @param limelightName
-     * @return
-     */
-    public static PoseEstimate getBotPoseEstimate_wpiBlue(String limelightName) {
-        return getBotPoseEstimate(limelightName, "botpose_wpiblue");
-    }
-
-    /**
      * Gets the Pose2d for easy use with Odometry vision pose estimator
      * (addVisionMeasurement)
      * 
@@ -687,16 +621,6 @@ public class LimelightHelpers {
         double[] result = getBotPose_wpiRed(limelightName);
         return toPose2D(result);
 
-    }
-
-    /**
-     * Gets the Pose2d and timestamp for use with WPILib pose estimator (addVisionMeasurement) when you are on the RED
-     * alliance
-     * @param limelightName
-     * @return
-     */
-    public static PoseEstimate getBotPoseEstimate_wpiRed(String limelightName) {
-        return getBotPoseEstimate(limelightName, "botpose_wpired");
     }
 
     /**
@@ -722,11 +646,6 @@ public class LimelightHelpers {
 
     public static void setPipelineIndex(String limelightName, int pipelineIndex) {
         setLimelightNTDouble(limelightName, "pipeline", pipelineIndex);
-    }
-
-    
-    public static void setPriorityTagID(String limelightName, int ID) {
-        setLimelightNTDouble(limelightName, "priorityid", ID);
     }
 
     /**
@@ -837,6 +756,12 @@ public class LimelightHelpers {
         return false;
     }
 
+    public static void initializeMapper() {
+        if (mapper == null) {
+            mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        }
+    }
+
     /**
      * Parses Limelight's JSON results dump into a LimelightResults Object
      */
@@ -844,6 +769,7 @@ public class LimelightHelpers {
 
         long start = System.nanoTime();
         LimelightHelpers.LimelightResults results = new LimelightHelpers.LimelightResults();
+        // THIS CAUSES LAG SO INITIALIZE IT BEFOREHAND WE SPENT FIVE HOURS TO FIND THIS
         if (mapper == null) {
             mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         }
@@ -851,7 +777,7 @@ public class LimelightHelpers {
         try {
             results = mapper.readValue(getJSONDump(limelightName), LimelightResults.class);
         } catch (JsonProcessingException e) {
-            results.error = "lljson error: " + e.getMessage();
+            System.err.println("lljson error: " + e.getMessage());
         }
 
         long end = System.nanoTime();
