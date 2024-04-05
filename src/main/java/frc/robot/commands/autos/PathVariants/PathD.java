649972445053bcf8632eff32a887c6c7254a766b
Kaden Bautista <k07bautista@gmail.com>
2024-04-05 13:54:50 -0700

    fixed path D so its the only one that uses vision

--- src/main/java/frc/robot/commands/autos/PathVariants/PathD.java
+++ src/main/java/frc/robot/commands/autos/PathVariants/PathD.java
@@ -22,27 +22,72 @@ public class PathD extends SequentialCommandGroup{
      * @param targetNoteArea target area for drive to note
      * @param paths paths in order you want to go
      */
-    public PathD(SwerveDrivetrain swerve, SuperSystem superSystem, NoteAssistance noteAssistance, double targetNoteArea, PathPlannerPath... paths) {
-        for (i = 0; i < paths.length; i++) {
-            addCommands(
-                AutoBuilder.followPath(paths[i]),
+    // public PathD(SwerveDrivetrain swerve, SuperSystem superSystem, NoteAssistance noteAssistance, double targetNoteArea, PathPlannerPath... paths) {
+    //     for (i = 0; i < paths.length; i++) {
+    //         addCommands(
+    //             AutoBuilder.followPath(paths[i]),
+    //             Commands.either(
+    //                 Commands.sequence(
+    //                     superSystem.intakeUntilSensedAuto(2),
+    //                     noteAssistance.driveToNoteCommand(swerve, targetNoteArea, 0, 0, 10, 50, new Pose2d())
+    //                         .onlyWhile(noteAssistance::hasTarget).until(superSystem::noteIntook),
+    //                     Commands.runOnce(() -> stop = superSystem.noteIntook() ? true : false)
+    //                 ),
+    //                 Commands.none(), 
+    //                 noteAssistance::hasTarget
+    //             )
+    //         );
+    //         if(stop) break;
+    //     }
+    // }
+
+    /**
+     * max is 2 hops
+     * @param swerve
+     * @param superSystem
+     * @param noteAssistance
+     */
+    public PathD(SwerveDrivetrain swerve, SuperSystem superSystem, NoteAssistance noteAssistance, double targetNoteArea, PathPlannerPath path1, PathPlannerPath path2) {
+        addCommands(
+            Commands.sequence(
                 Commands.either(
                     Commands.sequence(
                         superSystem.intakeUntilSensedAuto(2),
                         noteAssistance.driveToNoteCommand(swerve, targetNoteArea, 0, 0, 10, 50, new Pose2d())
-                            .onlyWhile(noteAssistance::hasTarget).until(superSystem::noteIntook),
-                        Commands.runOnce(() -> stop = superSystem.noteIntook() ? true : false)
+                            .onlyWhile(noteAssistance::hasTarget)
+                    ),
+                    Commands.none(), 
+                    noteAssistance::hasTarget
+                ),
+
+                AutoBuilder.followPath(path1),
+                Commands.either(
+                    Commands.sequence(
+                        superSystem.intakeUntilSensedAuto(2),
+                        noteAssistance.driveToNoteCommand(swerve, targetNoteArea, 0, 0, 10, 50, new Pose2d())
+                            .onlyWhile(noteAssistance::hasTarget)
+                    ),
+                    Commands.none(), 
+                    noteAssistance::hasTarget
+                ),
+
+                AutoBuilder.followPath(path2),
+                Commands.either(
+                    Commands.sequence(
+                        superSystem.intakeUntilSensedAuto(2),
+                        noteAssistance.driveToNoteCommand(swerve, targetNoteArea, 0, 0, 10, 50, new Pose2d())
+                            .onlyWhile(noteAssistance::hasTarget)
                     ),
                     Commands.none(), 
                     noteAssistance::hasTarget
                 )
-            );
-            if(stop) break;
-        }
+
+            ).until(superSystem::noteIntook)
+        );
     }
 
     /**
      * @return the path this part of the auto stopped at (ex. 1st path, 2nd path ...)
      */
-    public int getPathStoppedAt() { return i + 1; }
+    // public int getPathStoppedAt() { return i + 1; }
 }
